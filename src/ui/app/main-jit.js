d851eade7f72c8d6b79cf3b6af426cec5f91e845
firepick1 (localhost) <karl@firepick.org>
2017-04-27 15:41:28 -0700

    jit

--- src/ui/app/main-jit.js
+++ src/ui/app/main-jit.js
@@ -2,4 +2,4 @@
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var rest_bundle_module_1 = require("../app/rest_bundle.module");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(rest_bundle_module_1.RestBundleModule);
-//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main-jit.js.map
