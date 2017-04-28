394efd6730a56e3d8389c45efbb575cb0c4b7b25
firepick1 (localhost) <karl@firepick.org>
2017-04-28 12:51:15 -0700

    ts

--- src/ui/app/main-aot.js
+++
@@ -1,5 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { RestBundleModuleNgFactory } from '../aot/app/rest_bundle.module.ngfactory';
-console.log("AOT compiled");
-platformBrowser().bootstrapModuleFactory(RestBundleModuleNgFactory);
-//# sourceMappingURL=main-aot.js.map
--- src/ui/app/main-jit.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var rest_bundle_module_1 = require("../app/rest_bundle.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(rest_bundle_module_1.RestBundleModule);
-//# sourceMappingURL=main-jit.js.map
