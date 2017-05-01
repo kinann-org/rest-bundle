f6a673e5135184d6f8dc3412b6639d1e7c456889
firepick1 (localhost) <karl@firepick.org>
2017-04-30 17:25:00 -0700

    jit

--- test/rest-bundle.js
+++ test/rest-bundle.js
@@ -49,4 +49,32 @@ const supertest = require("supertest");
             });
         }).end((err,res) => {if (err) throw err; else done(); });
     })
+    it("GET /ui returns index HTML", function(done) {
+        var app = require("../scripts/hello-node.js");
+
+        supertest(app).get("/greeting/ui").expect((res) => {
+            res.statusCode.should.equal(302); // redirect
+            res.headers["content-type"].should.match(/text/);
+            res.headers["content-type"].should.match(/utf-8/);
+            res.headers["location"].should.equal("/greeting/ui/index-jit");
+        }).end((err,res) => {if (err) throw err; });
+
+        supertest(app).get("/greeting/ui/index-jit").expect((res) => {
+            res.statusCode.should.equal(200); 
+            res.headers["content-type"].should.match(/html/);
+            res.headers["content-type"].should.match(/utf-8/);
+            res.text.should.match(/<html>/);
+            res.text.should.match(/<index-link index="jit"/);
+            res.text.should.match(/service="greeting"/); // EJS injects service name
+        }).end((err,res) => {if (err) throw err; else done(); });
+    })
+    it("GET /ui/app returns Angular static content", function(done) {
+        var app = require("../scripts/hello-node.js");
+
+        supertest(app).get("/greeting/ui/app/main-jit.js").expect((res) => {
+            res.statusCode.should.equal(200); // redirect
+            res.headers["content-type"].should.match(/application\/javascript/);
+            res.text.should.match(/bootstrapModule/);
+        }).end((err,res) => {if (err) throw err; else done(); });
+    })
 })
