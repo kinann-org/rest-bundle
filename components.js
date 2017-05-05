fbc7d1c1a3f11d99631445192e6e226cbeeb63a7
firepick1 (localhost) <karl@firepick.org>
2017-05-04 18:10:24 -0700

    lib

--- components.js
+++ components.js
@@ -1,2 +1,1 @@
-exports.RbIdentity = require('./lib/rb-identity.component').RbIdentity;
 exports.RbModule = require('./lib/rb.module').RbModule;
--- examples/demo/src/ui/app/app.module.ts
+++ examples/demo/src/ui/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule }      from '@angular/core';
 import { HttpModule }      from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-//import { RbModule } from 'rest-bundle/components';
-import { RbIdentity } from 'rest-bundle/components';
+import { RbModule } from 'rest-bundle/components';
 
 import { AppComponent }  from './app.component';
 
@@ -11,15 +10,13 @@ import { AppComponent }  from './app.component';
     imports:      [ 
         BrowserModule,
         HttpModule,
-//        RbModule,
+        RbModule,
     ],
     declarations: [ 
-        RbIdentity,
         AppComponent,
     ],
     bootstrap:    [  // top-level
-        AppComponent, // must come first
-        //RbIdentity,  // imported library Components cannot be top-level
+        AppComponent, 
     ]
 })
 export class AppModule { 
