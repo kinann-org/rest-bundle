4e7e7ef73f0ca84730506230635cb15470f4c728
firepick1 (localhost) <karl@firepick.org>
2017-05-11 20:18:59 -0700

    vue

--- src/ui/RbIdentity.vue
+++ src/ui/RbIdentity.vue
@@ -1,35 +1,36 @@
 <template>
 
     <g-row class="rb-component rb-identity" >
-        <g-column> 
-            <g-symbol clickable @click='showDetail=!showDetail' >
+        <g-col> 
+            <g-icon clickable @click='showDetail=!showDetail' >
                 <span :class='"glyphicon "+(error? "glyphicon-remove text-danger" : "glyphicon-ok text-success")'></span>
-            </g-symbol> 
-        </g-column>
-        <g-text v-show="!showDetail"><code>/{{service}}</code> </g-text>
-        <g-column v-show="showDetail">
-            <g-row><g-label>Service:</g-label><g-text><code>/{{service}}</code></g-text></g-row>
-            <g-row><g-label>Component:</g-label>
+            </g-icon> 
+        </g-col>
+        <g-header v-show="!showDetail"><code>/{{service}}</code> </g-header>
+        <g-col v-show="showDetail">
+            <g-row><g-header>Service:</g-header><g-text><code>/{{service}}</code></g-text></g-row>
+            <g-row><g-header>Component:</g-header>
                 <g-text><code>&lt;{{selector}}&gt;</code> {{stateType}}#{{stateId}}</g-text></g-row>
-            <g-row><g-label>Description:</g-label>
+            <g-row><g-header>Description:</g-header>
                 <g-text><slot>Displays identity of "{{service}}" RestBundle service</slot></g-text></g-row>
             <g-row>
-                <g-label>REST:</g-label>
+                <g-header>REST:</g-header>
                 <g-text><a :href="origin+'/'+service+'/identity'" target="_blank">/{{service}}/identity</a> 
                     <span class="text-danger">{{error}}</span></g-text>
             </g-row>
             <g-row>
-                <g-label>View:</g-label>
+                <g-header>View:</g-header>
                 <g-text><a :href='"/"+service+"/ui"' target="_blank">/{{service}}/ui</a></g-text>
             </g-row>
-            <g-row><g-label>Package:</g-label><g-text>{{package}}@{{version}}</g-text></g-row>
-        </g-column>
+            <g-row><g-header>Package:</g-header><g-text>{{package}}@{{version}}</g-text></g-row>
+        </g-col>
     </g-row>
 
 </template>
 <script>
 
-    import grid from './grid/grid.vue';
+    var grid = require('vue-g-row-col');
+
     export default {
         mixins: [ require("./mixins/rb-service.js") ],
         data: function() {
