ff2cf383dff64caa9911d405e12e97884c977d4d
firepick1 (localhost) <karl@firepick.org>
2017-05-16 21:14:57 -0700

    vue

--- vue.js
+++ vue.js
@@ -1,7 +1,15 @@
 import RbIdentity from "./src/ui/RbIdentity.vue";
+import RbState from "./src/ui/RbState.vue";
+import TreeView from "./src/ui/TreeView.vue";
+import TreeViewItem from "./src/ui/TreeViewItem.vue";
+const RbService = require( "./src/ui/mixins/rb-service.js" );
 
 var components = {
     RbIdentity,
+    RbState,
+    RbService,
+    TreeView,
+    TreeViewItem,
 }
 function plugin(Vue, options) {
     Object.keys(components).forEach( key => Vue.component(key, components[key]));
