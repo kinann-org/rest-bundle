5a163623f600fc32baa3ef50f732dc1045876022
firepick1 (localhost) <karl@firepick.org>
2017-06-02 12:20:28 -0700

    RbWebSocket

--- src/rest-bundle.js
+++ src/rest-bundle.js
@@ -1,12 +1,54 @@
-const ResourceMethod = require("./resource-method");
-const path = require("path");
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const winston = require("winston");
-const ws = require("ws");
-
 (function(exports) {
+    const ResourceMethod = require("./resource-method");
+    const path = require("path");
+    const express = require("express");
+    const bodyParser = require("body-parser");
+    const winston = require("winston");
+    const WebSocket = require("ws");
+
+    class RbWebSocket {
+        constructor(restBundles, listener, options={}) {
+            if (!listener) {
+                throw new Error("RbWebSocket(restBundle,listener,options) listener is required");
+            }
+            this.restBundles = restBundles;
+            this.listener = listener;
+            this.wss = new WebSocket.Server({ server: listener });
+            this.sockets = new Set();
+            var port = listener.address().port;
+            winston.info("WebSocket listening on port:", port);
+            this.wss.on('connection', (ws, req) => {
+                const ip = req.connection.remoteAddress;
+                this.sockets.add(ws);
+                winston.debug("WebSocket connected client:", ip);
+                ws.on('close', () => {
+                    this.sockets.delete(ws);
+                    winston.debug('WebSocket disconnected client:', ip);
+                });
+            });
+            var pushStateInterval = options.pushStateInterval == null ? 1000 : options.pushStateInterval;
+            pushStateInterval && setInterval(() => this.pushState(), pushStateInterval);
+        }
+        pushData(type, data) {
+            data = typeof data === 'string' ? JSON.parse(data) : data;
+            var message = JSON.stringify({ type, data });
+            winston.debug("pushing ", message);
+            this.sockets.forEach((ws) => ws.send(message));
+        }
+        pushState() {
+            var state = this.restBundles.reduce((acc, rb) => {
+                return Object.assign(acc, {
+                    [rb.name]: rb.getState(),
+                });
+            }, {});
+            var stateStr = JSON.stringify(state);
+            if (this.stateStr != stateStr) {
+                this.pushData("state", stateStr);
+                this.stateStr = stateStr;
+            }
+        }
+    }
+
     class RestBundle {
         constructor(name, options = {}) {
             if (name == null) {
@@ -33,6 +75,10 @@ const ws = require("ws");
                 return handler.call(that, req, res, next);
             }
             return new ResourceMethod(method, name, thatHandler, mime);
+        }
+
+        static get RbWebSocket() {
+            return RbWebSocket;
         }
 
         get handlers() {
