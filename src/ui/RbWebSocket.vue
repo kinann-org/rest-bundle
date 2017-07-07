5df422c6b79920d523e25638c497d78262d83a6a
firepick1 (localhost) <karl@firepick.org>
2017-07-06 20:02:56 -0700

    api

--- src/ui/RbWebSocket.vue
+++ src/ui/RbWebSocket.vue
@@ -3,25 +3,30 @@
 <div >
     <rb-about v-if="about" :name="componentName">
         <p> Displays web socket connection status. Refreshing page after lost connection
-            will restore connecdtion if server is active.
+            will restore connecdtion if server is active. 
+            Clicking icon opens configuration dialog.
         </p>
         <rb-about-item name="about" value="false" slot="prop">Show this descriptive text</rb-about-item>
     </rb-about>
     <div class='rbws-example' v-if="about">
-        <v-btn :class="rbwsBtnClass" icon large hover 
-            @click.native.stop="openDialog()">
-            <v-icon v-if="isConnected" :class="rbwsBtnClass" x-large>http</v-icon>
-            <v-icon v-if="!isConnected" x-large>error</v-icon>
+        <v-btn v-if="isConnected" :class="rbwsBtnClass" icon large hover 
+            @click.native.stop="apiOpen()">
+            <v-icon :class="rbwsBtnClass" large>http</v-icon>
         </v-btn>
+        <v-btn v-if="!isConnected"
+            class="red darken-4 white--text headline" icon large hover 
+            @click.native.stop="apiOpen()">&#x26a0;</v-btn>
     </div>
     <div class="rbws-container" v-if="!about">
-        <v-btn :class="rbwsBtnClass" icon large hover 
-            @click.native.stop="openDialog()">
-            <v-icon v-if="isConnected" :class="rbwsBtnClass" large>http</v-icon>
-            <v-icon v-if="!isConnected" large>error</v-icon>
+        <v-btn v-if="isConnected" :class="rbwsBtnClass" icon large hover 
+            @click.native.stop="apiOpen()">
+            <v-icon :class="rbwsBtnClass" large>http</v-icon>
         </v-btn>
+        <v-btn v-if="!isConnected"
+            class="red darken-4 white--text headline" icon large hover 
+            @click.native.stop="apiOpen()">&#x26a0;</v-btn>
     </div>
-    <v-dialog v-model="dialog" lazy persistent absolute width="90%">
+    <v-dialog v-model="apiDialog" lazy persistent absolute width="90%">
       <v-card>
         <v-card-title >messages: {{pushCount}}</v-card-title>
         <v-card-text v-if="isConnected">
@@ -29,23 +34,21 @@
                 v-model='api.pushStateMillis'
                 label="pushStateMillis" ></v-text-field>
         </v-card-text>
-        <v-card style="margin: 5px">
-        <v-card-text v-if="saveApiError" raised hover class="subheading error-card">
-            {{saveApiError.response.data.error}}
+        <v-card-text v-if="apiSaveError" raised hover class="subheading error-card">
+            {{apiSaveError.response.data.error}}
         </v-card-text>
         <v-card-text v-if="!isConnected" raised hover class="subheading error-card">
             Connection lost. Refresh when server is available.
         </v-card-text>
-        </v-card>
         <v-card-actions>
           <v-spacer></v-spacer>
+          <v-btn class="green--text darken-1" flat="flat" @click.native="apiCancel()">Cancel</v-btn>
           <v-btn class="green--text darken-1" flat="flat" 
             v-if="!isConnected"
-            @click.native="reload()">Refresh</v-btn>
-          <v-btn class="green--text darken-1" flat="flat" @click.native="dialog=false">Cancel</v-btn>
+            @click.native="apiRefresh()">Refresh</v-btn>
           <v-btn v-if="isConnected"
             class="green--text darken-1" flat="flat" 
-            @click.native="saveApi({rbhash:'nono'})">Save</v-btn>
+            @click.native="apiSave(api)">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -82,32 +85,35 @@ const Vue = require("vue").default;
                 var rbws = srb && srb['web-socket'];
                 rbws && Vue.set(rbws, 'showDetail',  !rbws.showDetail);
             },
-            reload() {
-                this.dialog = false;
+            apiRefresh() {
+                this.apiDialog = false;
                 window.location.reload();
             },
-            openDialog() {
-                this.dialog = true;
-                this.saveApiError = null;
+            apiOpen() {
+                var model = this.restBundleModel();
+                this.api = Object.assign(this.api, model.api);
+                this.apiDialog = true;
+                this.apiSaveError = null;
             },
-            saveApi(api) {
-                var that = this;
-                that.saveApiError = null;
-                var url = that.restOrigin() + "/" + that.service + "/" + that.model;
-                that.$http.put(url, { api })
+            apiCancel() {
+                this.apiDialog = false;
+            },
+            apiSave(api) {
+                this.apiSaveError = null;
+                var url = this.restOrigin() + "/" + this.service + "/" + this.model;
+                this.$http.put(url, { api })
                 .then(res => {
-                    var model = that.restBundleModel();
+                    var model = this.restBundleModel();
                     if (model) {
-                        that.$store.commit(['restBundle', that.service, that.model, 'update'].join('/'), res.data);
-                        that.api = res.data.api;
-                        that.dialog = false;
+                        this.$store.commit(['restBundle', this.service, this.model, 'update'].join('/'), res.data);
+                        this.api = res.data.api;
+                        this.apiDialog = false;
                     } else {
                         throw new Error("sadness");
                     }
                 })
                 .catch(err => {
-                    var data = err.response.data;
-                    that.saveApiError = err;
+                    this.apiSaveError = err;
                 });
             },
             wsOnMessage(event) {
@@ -137,7 +143,7 @@ const Vue = require("vue").default;
             rbwsBtnClass() {
                 var c = '';
                 if (this.isConnected) {
-                    c += 'grey darken-4 green--text mt-2'
+                    c += 'grey darken-4 green--text'
                     c += (this.pushCount % 2) ? ' text--darken-1' : ' text--lighten-2';
                 } else {
                     c += 'red white--text';
@@ -149,8 +155,8 @@ const Vue = require("vue").default;
             return {
                 webSocket: this.webSocket,
                 isConnected: null,
-                dialog: false,
-                saveApiError: null,
+                apiDialog: false,
+                apiSaveError: null,
                 hover: false,
                 api: {
                     pushStateMillis: 'loading...',
@@ -188,58 +194,15 @@ const Vue = require("vue").default;
     color: white;
     background-color: #b71c1c;
     border-top: 1pt solid #fff;
+    margin-left: 1em;
+    margin-right: 1em;
 }
 .rbws-container {
     position: relative;
-    padding-top: 1em;
+    padding-top: 1.5em;
 }
 .rbws-example {
     display: flex;
-    z-index: 99999;
-}
-.rbws-icon {
-    border-bottom-left-radius: 1em;
-    height: 1em;
-    width: 1em;
-}
-.rbws-icon:hover {
-    cursor: pointer;
-}
-.rbws-icon-terse {
-}
-.rbws-icon-detail {
-    border-bottom-left-radius: 0.5em;
-    height: 3.8em;
-}
-.rbws-icon-active0 {
-    background-color: rgba(35,155,86,1);
-}
-.rbws-icon-active1 {
-    background-color: rgba(35,155,86,0.8);
-}
-.rbws-icon-dead {
-    background-color: rgba(255,0,0,1);
-}
-.rbws-no-connection {
-    width: 10em;
-    color: #fff;
-    font-weight: 700;
-    background-color: rgba(255,0,0,1);
-    padding-top: 0.25em;
-    padding-left: 0.5em;
-    padding-right: 0.5em;
-}
-.rbws-text {
-    padding-top: 0.25em;
-    padding-left: 0.5em;
-    padding-right: 0.5em;
-    background-color: #ddd;
-}
-.fade-enter-active, .fade-leave-active {
-    transition: opacity .2s
-}
-.fade-enter, .fade-leave-to {
-    opacity: 0
 }
 </style>
 
