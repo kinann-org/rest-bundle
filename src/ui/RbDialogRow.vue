69dd4816e2f1ca2e41cc673cd92655c45e34fba6
firepick1 (localhost) <karl@firepick.org>
2017-07-21 14:20:08 -0700

    rb-dialog-row

--- src/ui/RbDialogRow.vue
+++ src/ui/RbDialogRow.vue
@@ -6,15 +6,22 @@
         </p>
         <rb-about-item name="about" value="false" slot="prop">Show this descriptive text</rb-about-item>
         <rb-about-item name="label" value="(label?)" slot="prop">Row label</rb-about-item>
-        <rb-about-item name="default" value="required" slot="slot">Dialog fields bound to apiSvc.apiModel fields</rb-about-item>
+        <rb-about-item name="default" value="required" slot="slot">Dialog fields bound to <var>apiSvc.apiModel</var> fields</rb-about-item>
+        <rb-about-item name="label" value="(optional)" slot="slot">For structured labels</rb-about-item>
+        <rb-about-item name="v-layout" value="(optional)" slot="slot"><code>v-flex</code> enclosed dialog field(s) for multi-field rows</rb-about-item>
     </rb-about>
     <v-layout row wrap>
         <v-flex xs3 class="rb-dialog-row-label">
             <v-subheader ><slot name="label">{{label}}</slot></v-subheader>
         </v-flex>
         <v-flex >
-            <slot></slot>
+            <slot>
+                <div v-if="about" class="rb-dialog-row-text">Individual dialog fields go inside default slot</div>
+            </slot>
         </v-flex>
+        <slot name="v-layout">
+            <div v-if="about" class="rb-dialog-row-text">Multiple dialog fields go inside <var>v-layout</var> slot</div>
+        </slot>
     </v-layout>
 </div>
 
